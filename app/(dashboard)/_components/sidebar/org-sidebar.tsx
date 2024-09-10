62a6ed86a840423c317b613838b77f53197b728a
Tim <tas57atx@gmail.com>
2024-09-10 16:19:25 -0500

    9/10/24 some errors

--- app/(dashboard)/_components/sidebar/org-sidebar.tsx
+++ app/(dashboard)/_components/sidebar/org-sidebar.tsx
@@ -1,10 +1,9 @@
 'use client'
 
-import React from 'react'
 
 export const OrgSidebar = () => {
   return (
-    <div className='hidden lg:flex flex-col space-y-6 w-[206px] pl-5pt-5 bg-red-500'>
+    <div className='hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-red-500'>
         Org Sidebar
     </div>
   )
