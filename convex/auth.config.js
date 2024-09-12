09eeefd67674e18a6104b24884fcb0135e7b4a35
Tim <tas57atx@gmail.com>
2024-09-12 12:48:48 -0500

    update 9/12/24

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
     providers: [
       {
-        domain: "https://brave-cowbird-87.clerk.accounts.dev",
+        domain: "https://brave-cowbird-87.clerk.accounts.dev/.well-known/jwks.json",
         applicationID: "convex",
       },
     ]
--- providers/convex-client-provider.tsx
+++ providers/convex-client-provider.tsx
@@ -1,40 +1,54 @@
 'use client';
 
-import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { AuthLoading, Authenticated, ConvexReactClient } from 'convex/react';
 import { Loading } from "@/components/auth/loading";
-import { ReactNode } from 'react';
 
 const PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
     throw new Error("Missing Publishable Key")
-   }
+   } 
    
 
 interface ConvexClientProviderProps {
-    children: ReactNode;
+    children: React.ReactNode;
 };
 
 const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
 
-const convex = new ConvexReactClient(convexUrl);
+const convex = new ConvexReactClient(convexUrl)
 
 export const ConvexClientProvider = ({
     children,
 }: ConvexClientProviderProps) => {
-       return (
-            <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+
+        return (
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
                 <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
-                    <Authenticated>
-                    {children}    
-                    </Authenticated>
                         <AuthLoading>
                             <Loading />
                         </AuthLoading>
+                        <Authenticated>
+                            {children}    
+                        </Authenticated>
                 </ConvexProviderWithClerk>
-            </ClerkProvider>
-        );
-    };
+            </ClerkProvider> 
+      
+
+    
+    /*        ReactDOM.createRoot(document.getElementById('root')!).render(
+        <React.StrictMode>
+          <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+                {children}
+            </ConvexProviderWithClerk>
+          </ClerkProvider>
+        </React.StrictMode>,
+      ) */
+    ); 
+};
     
